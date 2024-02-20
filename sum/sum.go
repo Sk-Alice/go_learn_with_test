5eef3e8f12fa9adbc4d661a0d11b5369b853875a
Sk-Alice <3022844681@qq.com>
2024-02-20 22:05:34 +0800

    modify sum

--- sum/sum.go
+++ sum/sum.go
@@ -1,8 +1,29 @@
 package sum
 
-func Sum(numbers [5]int) (sum int) {
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
+func Sum(numbers []int) (sum int) {
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersOfSum ...[]int) (sums []int) {
+	for _, numbers := range numbersOfSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
 	}
 	return
 }
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,14 +1,57 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if want != got {
+		got := Sum(numbers)
+		want := 15
+
+		if want != got {
+			t.Errorf("want '%d' but got '%d'", want, got)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	// 在 Go 中不能对切片使用等号运算符。你可以写一个函数迭代每个元素来检查它们的值。
+	// 但是一种比较简单的办法是使用 reflect.DeepEqual，它在判断两个变量是否相等时十分有用。
+	// 非安全类型
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("want '%d' but got '%d'", want, got)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want '%d' but got '%d'", want, got)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+
 }
