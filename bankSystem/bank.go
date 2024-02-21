0be47db41491a192fbc15142835d7c5ed7ed7f38
Sk-Alice <3022844681@qq.com>
2024-02-21 10:36:16 +0800

    add Withdraw

--- bankSystem/bank.go
+++ bankSystem/bank.go
@@ -16,10 +16,17 @@ func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
+// Deposit 存钱
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
+// Withdraw 取钱
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+// Balance 查看金额
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
